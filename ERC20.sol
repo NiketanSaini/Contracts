e05f927ac3a0db6f92f30829357bfc3b06fdab09
Niketan <55307223+NiketanSaini@users.noreply.github.com>
2020-10-18 15:45:32 +0530

    Create ERC20.sol

---
+++ ERC20.sol
@@ -0,0 +1,61 @@
+pragma solidity ^0.6.0;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/math/SafeMath.sol";
+ contract ERC20{
+     using SafeMath for uint256;
+     uint256 totalSupply_;
+     
+     constructor(uint256 total) public {
+         totalSupply_ = total;
+         balances[msg.sender] = totalSupply_;
+         
+     }
+     
+     mapping(address => uint256) balances;
+     
+     mapping(address => mapping (address => uint256)) allowed;
+     
+     event Approval(address indexed tokenOwner, address indexed spender,uint tokens);
+     
+     event Transfer(address indexed from, address indexed to,uint tokens);
+     
+     function totalSupply() public view returns (uint256) {
+         return totalSupply_;
+         
+     }
+     
+     function balanceOf(address tokenOwner) public view returns (uint) {
+         return balances[tokenOwner];
+         
+     }
+     
+     function transfer(address receiver,uint numTokens) public returns (bool) {
+         require(numTokens <= balances[msg.sender]);
+         balances[msg.sender] = balances[msg.sender].sub(numTokens);
+         balances[receiver] = balances[receiver].add(numTokens);
+         emit Transfer(msg.sender, receiver, numTokens);
+         return true;
+         
+     }
+     
+     function approve(address delegate,uint numTokens) public returns (bool) {
+         allowed[msg.sender][delegate] = numTokens;
+         emit Approval(msg.sender, delegate, numTokens);
+         return true;
+         
+     }
+     
+     function allowance(address owner,address delegate) public view returns (uint) {
+         return allowed[owner][delegate];
+         
+     }
+     function transferFrom(address owner, address buyer,uint numTokens) public returns (bool) {
+         require(numTokens <= balances[owner]);
+         require(numTokens <= allowed[owner][msg.sender]);
+         balances[owner] = balances[owner].sub(numTokens);
+         allowed[owner][msg.sender] = allowed[owner][msg.sender].sub(numTokens);
+         balances[buyer] = balances[buyer].add(numTokens);
+         Transfer(owner, buyer, numTokens);
+         return true;
+         
+     }
+ }
